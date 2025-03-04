f59f44e7c5f09a47b4267a1d2a3cf1bab7f1e9a2
victoriag2797 <victoriag2797@tuckahoeschools.org>
2025-03-04 13:32:45 +0000

    init commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
-function setup(){
-    createCanvas(400, 400);
-}
-
-function draw(){
-    background(255, 0 , 200);
-}
+function setup() {
+    createCanvas(100, 100, WEBGL);
+  
+    background(200);
+  
+    circle(0, 0, 25);
+  
+    describe('A white circle with black outline in the middle of a gray canvas.');
+  }
