a4327b3f7238f409e99aa3dc85d53b5856318c1c
victoriag2797 <victoriag2797@tuckahoeschools.org>
2025-03-07 13:32:41 +0000

    init commit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
+// Set the video capture as a global variable.
+let capture;
+
 function setup() {
-    createCanvas(100, 100, WEBGL);
-  
-    background(200);
-  
-    circle(0, 0, 25);
-  
-    describe('A white circle with black outline in the middle of a gray canvas.');
-  }
+  describe('Video capture from the device webcam.');
+  createCanvas(1572, 718);
+
+  // Use the createCapture() function to access the device's
+  // camera and start capturing video.
+  capture = createCapture(VIDEO);
+
+  // Make the capture frame half of the canvas.
+  capture.size(2000, 1000);
+
+  // Use capture.hide() to remove the p5.Element object made
+  // using createCapture(). The video will instead be rendered as
+  // an image in draw().
+  capture.hide();
+}
+
+function draw() {
+  // Set the background to gray.
+  background(51);
+
+  // Draw the resulting video capture on the canvas
+  // with the invert filter applied.
+  image(capture, 0, 0, 360, 400);
+}
